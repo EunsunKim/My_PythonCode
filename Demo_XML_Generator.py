69e9675c42a276282a6a3d627b9c37494a32f142
Cecilia <EunsunKim@users.noreply.github.com>
2021-03-21 18:37:15 -0400

    Add files via upload
    update descriptions for parameters and print format for an easy readable

--- Demo_XML_Generator.py
+++ Demo_XML_Generator.py
@@ -5,34 +5,33 @@ class DictMaker(dict):
 		self[key] = value
 
 
-def parseFileToDict(fileName, targetString):
-	handler = open(fileName, 'r').read()
-	content = bs(handler, 'lxml-xml')
+def parseFileToDict(input1, input2):
+	"""
+	:param input1: The string which is to be reversed.
+	:param input2: The string which is to be reversed.
+	:return: The string which gets reversed.
+	"""
+	handler = open(input1, 'r').read()
+	content = bs(handler, 'lxml-xml')  # if not working, try pip python install lxml
 
 	dict_object = DictMaker()
 	i = 0
 
-	for tag in content.findAll(targetString):
-		dict_object.add(i, {'Platform' : tag.Platform.text,
-		                    'TItle' : tag.Title.text,
-		                    'Developer' : tag.Developer.text,
-		                    'Publisher' : tag.Publisher.text,
-		                    'Director' : tag.Director.text,
-		                    'Producer' : tag.Producer.text,
-		                    'Genre' : tag.Genre.text,
-		                    'ESRB' : tag.ESRB.text,
-		                    'Release' : tag.Release.text
-		                    })
+	for tag in content.findAll(input2):
+		dict_object.add(i, {
+			'Platform': tag.Platform.text, 'Title': tag.Title.text, 'Developer': tag.Developer.text,
+			'Publisher': tag.Publisher.text, 'Director': tag.Director.text,
+			'Producer': tag.Producer.text, 'Genre': tag.Genre.text, 'ESRB': tag.ESRB.text, 'Release': tag.Release.text})
 		i += 1
 	return dict_object
 
 
-
 if __name__ == "__main__":
-	fileName ='Game_catalog.xml'
+	fileName = 'Game_catalog.xml'
 	targetString = 'GameCollection'
 
 	dictData = parseFileToDict(fileName, targetString)
-	for k,v in dictData.items():
+	for k, v in dictData.items():
 		if 'PS4' in v.get('Platform'):
-			print( v.get('Platform'), v.get('TItle'), v.get('ESRB'))
+			print(
+				('{}' + '\t\tRate : {}' + '\t\t[ Title ] {}').format(v.get('Platform'), v.get('ESRB'), v.get('Title')))
